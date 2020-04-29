94d9cc230a0582221d131694da5ddd6d0044ce3d
ligang <ligang@example.com>
2020-04-29 11:08:34 +0800

    sd

--- miniprogram/pages/cloud/cloud.js
+++ miniprogram/pages/cloud/cloud.js
@@ -8,13 +8,13 @@ Page({
    * 页面的初始数据
    */
   data: {
+    
     items: [
       { name: 'USA', value: '美国' },
       { name: 'CHN', value: '中国', checked: 'true' },
       { name: 'BRA', value: '巴西' },
       { name: 'JPN', value: '日本' },
       { name: 'ENG', value: '英国' },
-      { name: 'TUR', value: '法国' },
     ],
     
     imgages:[]
